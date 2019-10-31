c0a739154977dc6512339deec444ad4d502e8b46
Jesper Petersen <info@ordklar.dk>
2019-10-31 15:35:57 +0100

    add more

--- src/main/kotlin/threads.kt
+++ src/main/kotlin/threads.kt
@@ -4,22 +4,22 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
-fun threadsCount(limit: Int)
+fun threadsCount(limit: Int, step: Int)
 {
     for (i in 1.. limit) thread {
         Thread.sleep(100)
-        print("T$i ")
+        if(i%step == 0) print("T$i ")
     }
     println()
 }
 
-fun doCoroutines(limit: Int)
+fun doCoroutines(limit: Int, Step: Int)
 {
     runBlocking {
         for (i in 1..limit) {
             launch {
                 delay(100)
-                print("A $i")
+                if(i%Step == 0) print("A$i ")
             }
         }
         println()
@@ -27,6 +27,6 @@ fun doCoroutines(limit: Int)
 }
 
 fun  main(){
-    doCoroutines(100)
-    threadsCount(100)
+    doCoroutines(10000, 1000)
+    threadsCount(10000000, 1000)
 }
